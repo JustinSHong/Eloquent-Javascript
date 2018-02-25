1c580f7018857a1cc260a40ae5be9d1b99f6063b
Justin Hong <jhong2009@gmail.com>
2018-02-24 18:18:34 -0800

    upload minimum exercise

---
+++ Ch3Minimum.js
@@ -0,0 +1,9 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(a, b) {
+
+	return Math.min(a, b);
+}
+
+console.log(min(0, 10)); // 0
+console.log(min(0, -10)); // -10
