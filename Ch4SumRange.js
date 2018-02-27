7de46ee6ae01a486431651777af2065c48614b00
Justin Hong <jhong2009@gmail.com>
2018-02-26 21:45:54 -0800

    upload alternative sum() using arr.reduce()

--- Ch4SumRange.js
+++ Ch4SumRange.js
@@ -31,6 +31,12 @@ function sum (arr) {
 	return sum;
 }
 
+function sum (arr) {
+	return arr.reduce(function(a, b) {
+		return a + b;
+	});
+}
+
 range(1, 10); // [1,2,3,4,5,6,7,8,9,10]
 sum(range(1, 10)); // 55
 
