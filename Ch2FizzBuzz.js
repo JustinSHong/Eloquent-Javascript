9980c9a1c6e7e5508a482d59f76fceefb4f4b307
Justin Hong <jhong2009@gmail.com>
2018-03-15 15:09:25 -0700

    refactored code block in for loop

--- Ch2FizzBuzz.js
+++ Ch2FizzBuzz.js
@@ -6,19 +6,20 @@
 // for numbers that are divisible by both 3 and 5.
 
 function fizzBuzz() {
-
+	
+	// loop from 1 to 100
 	for (let i = 1; i < 101; i++) {
-		if (i % 3 === 0) {
-			if (i % 5 === 0) {
-				console.log('FizzBuzz');
-			} else {
-				console.log('Fizz');
-			}	
+		// check if i is a divisible by 3 and 5
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log('FizzBuzz');
 		}
+		// is i divisible by 3
+		else if (i % 3 === 0) {
+			console.log('Fizz');
+		}
+		// is i divisble by 5
 		else if (i % 5 === 0) {
 			console.log('Buzz');
-		} else {
-			console.log(i);	
 		}
 	}
 }
