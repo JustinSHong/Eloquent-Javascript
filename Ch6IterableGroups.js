607c52b9a3fb9edd1832d7c99b31c2c47dcadd2e
Justin Hong <jhong2009@gmail.com>
2018-03-25 18:42:29 -0700

    added Group class and set up next() method in GroupIterator

--- Ch6IterableGroups.js
+++ Ch6IterableGroups.js
@@ -8,7 +8,50 @@
 
 // It is okay if your iterator behaves strangely when the group is modified during iteration.
 
+class Group {
+	constructor(group) {
+		this.group = [];
+	}
+	// Prototype methods
+	add(value) {
+		// add a value to a Group only if it is unique
+	  if (!this.has(value)) {
+	  	this.group.push(value);
+	  }
+	}
+	delete(value) {
+		// remove an argument from the group if it was a member
+		if (this.has(value)) {
+			let idx = this.group.indexOf(value);
+			return this.group.splice(idx, 1);
+		}
+	}
+	has(value) {
+		// returns a Boolean indicating if a value was a member
+		return this.group.includes(value);
+	}
+	// Constructor method - does not belong to Group prototype
+	static from(object) {
+		// create a group with all values produced by iterating over it
+		let newGroup = new Group({
+			group: []
+		});
+		object.forEach((value) => newGroup.group.push(value));
+		return newGroup;
+	}
+}
 
+// instances of GroupIterator tracks the current position in the group
+class GroupIterator {
+	constructor(position) {
+		this.position = 0;
+	}
+	// Prototype methods
+  next() {
+  	// check if last value in the group has been called
+  	// if not, iterate position and return the next value
+  }
+}
 
 
 // test cases
